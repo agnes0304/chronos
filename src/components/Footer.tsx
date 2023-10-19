a7e4407ba3648d9c62f18c894b64f139556f7551
agnes0304 <jiwoochoi0304@gmail.com>
2023-10-19 14:31:38 +0900

    footer ongoing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,23 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import naverCircle from "../assets/naverCircle.png";
+
 const Footer = () => {
   return (
-    <>
-      <div>Footer</div>
-    </>
+    <footer className="fixed bottom-0 left-0 w-[100vw] py-4 bg-gray-100 flex justify-center items-center">
+      <div className="flex gap-2 items-center">
+        <a className="text-black text-lg" target="_blank" href="https://github.com/agnes0304">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a target="_blank" href="https://blog.naver.com/loghistory">
+          <img
+            className="w-[18px] h-[18px]"
+            src={naverCircle}
+            alt="naver blog logo"
+          />
+        </a>
+      </div>
+    </footer>
   );
 };
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
 const Header = () => {
   return (
-    <>
-      <div>Header</div>
-    </>
+    <header className="fixed top-0 left-0 w-[100vw] py-4 flex justify-center items-center">
+      <div>
+        <p className="text-sm text-gray-300 w-[90vw]">필기깎는화석</p>
+      </div>
+    </header>
   );
 };
 
