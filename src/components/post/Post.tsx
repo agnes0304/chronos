fb7ed5161d9b636fd83940ed7a84344499461ec4
agnes0304 <jiwoochoi0304@gmail.com>
2023-10-20 19:03:07 +0900

    added img hover effect

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import Actions from "./Actions";
-
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import summary from "../../assets/summary.webp";
 
 // TODO: post link, Actions로 넘기기
@@ -8,8 +9,16 @@ import summary from "../../assets/summary.webp";
 const Post: FC = () => {
   return (
     <div className="flex w-full h-[120px] border rounded border-gray-300 p-1 px-2">
-      <img src={summary} />
-      {/* TODO: img hover -> 돋보기 / onClick -> 모달 open/ 확대 */}
+      <div className="relative w-auto h-full group shrink-0">
+        <img
+          className="w-full h-full overflow-hidden object-cover hover:opacity-30 transition-opacity duration-300"
+          src={summary}
+        />
+        <div className="absolute text-2xl text-gray-400 pointer-events-none opacity-0 top-[35%] left-[42%] group-hover:opacity-100 transition-opacity duration-300">
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </div>
+      </div>
+      {/* TODO: onClick -> 모달 open + 확대 */}
       <div className="flex flex-col justify-between gap-1 ml-2 mb-1">
         <h1 className="text-lg text-gray-400">title</h1>
         <p className="text-sm text-gray-400 overflow-auto">
